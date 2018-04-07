a9dece13091a03a254effd6c5946b518f28a6395
sallzzbr <evio.salgado@hotmail.com>
2018-04-07 16:52:48 -0300

    connected app.js to index.html

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var ViewModel = function() {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+  this.imgAttribution = ko.observable('www.google.com');
+
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1);
+  };
+
+}
+
+ko.applyBindings(new ViewModel());
